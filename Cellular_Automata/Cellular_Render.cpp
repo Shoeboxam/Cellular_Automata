9a46fd32f532b3cfcea3ec654cecf71ea4dba8f9
Shoeboxam <shoeboxam@gmail.com>
2014-09-29 03:01:03 -0500

    File placement, don't exclude render

--- Cellular_Automata/Cellular_Render.cpp
+++ Cellular_Automata/Cellular_Render.cpp
@@ -1,7 +1,17 @@
-#ifndef Cellular_Render.h
-#define Cellular_Render.h
-
 #include "Cellular_Render.h"
+
+
+cellular_render::cellular_render(automaton *user_automaton){
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(320, 320);   // Set the window's initial width & height
+	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+	glutCreateWindow("Cellular Automata");
+	glutDisplayFunc(display); // Register display callback handler for window re-paint
+	glutMainLoop();           // Enter the infinitely event-processing loop
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+}
 
 void cellular_render::display(automaton *user_automaton) {
 	float offsetx = 1 / width;
